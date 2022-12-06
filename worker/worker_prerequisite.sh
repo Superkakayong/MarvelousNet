592e764dd542be8976b9e636415810641c1a13a8
yantong <yantong@bu.edu>
2022-12-05 22:10:25 -0500

    update worker script

--- worker/worker_prerequisite.sh
+++ worker/worker_prerequisite.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/yzhang37/655ImageMiniProject/main/worker/imagenet_label.json
-wget https://raw.githubusercontent.com/yzhang37/655ImageMiniProject/main/worker/worker.py
+wget https://download.pytorch.org/models/googlenet-1378be20.pth
+wget https://raw.githubusercontent.com/Superkakayong/MarvelousNet/master/worker/imagenet_label.json
+wget https://raw.githubusercontent.com/Superkakayong/MarvelousNet/master/worker/worker.py
+wget https://raw.githubusercontent.com/Superkakayong/MarvelousNet/master/worker/requirements.txt
+
 sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo apt-get install -y libjpeg-dev zlib1g-dev
 pip3 --no-cache-dir install torch==1.10.0+cpu torchvision==0.11.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+pip3 install -r requirements.txt
